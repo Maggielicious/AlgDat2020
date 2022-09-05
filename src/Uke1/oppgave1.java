a436e8ab1ff933d8ad8b8b3cab1dc58300700b10
Maggielicious <magnus.belsvik@gmail.com>
2022-09-05 09:57:16 +0200

    Initial commit

---
+++ src/Uke1/oppgave1.java
@@ -0,0 +1,13 @@
+package Uke1;
+
+public class oppgave1 {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+
+        System.out.println("Teller til til");
+
+        for(int i =1; i<=10; i++) {
+            System.out.println(i);
+        }
+    }
+}
