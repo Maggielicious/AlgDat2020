867d2416cd38bb86f11b104f39157d9b87be632a
Maggielicious <magnus.belsvik@gmail.com>
2022-09-05 10:06:58 +0200

    hgjhg

--- src/Uke1/oppgave1.java
+++ src/Uke1/oppgave1.java
@@ -4,9 +4,14 @@ public class oppgave1 {
     public static void main(String[] args){
         System.out.println("Hello world");
 
-        System.out.println("Teller til til");
+        System.out.println("Teller til ti");
 
         for(int i =1; i<=10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Petra til tjuve");
+
+        for(int i =11; i<=20; i++) {
             System.out.println(i);
         }
     }
